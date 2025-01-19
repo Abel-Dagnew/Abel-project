278f889e1ea630a2f6c2b09238bd07c9ee3cecf9
Abel-Dagnew <abeltesfaye111@gmail.com>
2025-01-19 19:18:44 +0100

    added backend

--- main.tf
+++ main.tf
@@ -6,12 +6,12 @@ terraform {
     }
     
   }
-  # backend "azurerm" {
-  #   resource_group_name  = "abel_RG"
-  #   storage_account_name = "terraform443"
-  #   container_name       = "terraform-statefile"
-  #   key                  = "terraform.tfstate"
-  # }
+  backend "azurerm" {
+    resource_group_name  = "abel_RG"
+    storage_account_name = "terraform443"
+    container_name       = "terraform-statefile"
+    key                  = "terraform.tfstate"
+  }
 }
 
 
