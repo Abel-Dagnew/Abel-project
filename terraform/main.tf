9d9c5b420264a9b653fe72229571bde15268ac8d
Abelvm <Abeltesfaye111@gmail.com>
2025-05-27 12:57:06 +0000

    added terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,14 @@
 terraform {
+
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"   # specify your desired version
+    }
+  }
+
   
   backend "azurerm" {
     resource_group_name  = "abel_RG"
